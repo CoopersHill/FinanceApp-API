1ea513fabf2052119bce5773f1c67810023ce025
Hanif Warren <Hanif.warren@gmail.com>
2020-11-29 13:07:00 -0500

    Add project files.

---
+++ hwFinanceApp/Models/FinanceContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace hwFinanceApp.Models
+{
+    public class FinanceContext : DbContext
+    {
+        public FinanceContext(DbContextOptions<FinanceContext> options) : base(options) 
+        {         
+        }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
---
+++ hwFinanceApp/Models/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hwFinanceApp.Models
+{
+    public class Transaction
+    {
+        public long Id { get; set; }
+        public string ItemName { get; set; }
+        public double ItemCost { get; set; }
+        public bool IsPurchanse { get; set; }
+
+    }
+}
