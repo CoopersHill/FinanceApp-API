7528cff648f1b1aeaadb7e165aa5b3d910bd25c4
Hanif Warren <Hanif.warren@gmail.com>
2020-12-21 23:13:37 -0500

    Refactored Transactions class per ERD

--- hwFinanceApp/Models/Transaction.cs
+++ hwFinanceApp/Models/Transaction.cs
@@ -8,11 +8,15 @@ namespace hwFinanceApp.Models
     public class Transaction
     {
         public long Id { get; set; } //primary key of transaction
-        public string ItemName { get; set; }
-        public double ItemCost { get; set; }
-        public bool RecStatus { get; set; }        
-        public DateTime TransactionDate { get; set; }
         public int BankAccountId { get; set; } //Id to which the transaction belongs.
-
+        public string Description { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public double Amount { get; set; }
+        public string Type { get; set; }
+        public int CategoryId { get; set; }
+        public string EnteredById { get; set; }
+        public bool RecStatus { get; set; }        
+        public double ReconciledAmount { get; set; }
+        public int EnteredBy_Id { get; set; }
     }
 }
