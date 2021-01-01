d7cb1eb3e411ba60dff1491e7ef87222c88974cf
Hanif Warren <Hanif.warren@gmail.com>
2021-01-01 17:58:24 -0500

    Configured sql server datatbase and added all models to finance Context. Added localdb connection string. Next step is initializing the DB with test data (see https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/intro?view=aspnetcore-5.0#ef-core-nuget-packages)

--- hwFinanceApp/Models/FinanceContext.cs
+++ hwFinanceApp/Models/FinanceContext.cs
@@ -15,8 +15,18 @@ namespace hwFinanceApp.Models
         public DbSet<Budget> Budgets { get; set; }
         public DbSet<BudgetItem> BudgetItems { get; set; }
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Transaction> Transactions { get; set; }
         public DbSet<HouseHold> HouseHolds { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BankAccount>().ToTable("BankAccounts"); //only useful to override property name tables to force plural
+            modelBuilder.Entity<Budget>().ToTable("Budgets");
+            modelBuilder.Entity<BudgetItem>().ToTable("BudgetItems");
+            modelBuilder.Entity<Category>().ToTable("Categories");
+            modelBuilder.Entity<HouseHold>().ToTable("Households");
+            modelBuilder.Entity<Transaction>().ToTable("Transactions");
+        }
         
     }
 }
--- hwFinanceApp/Startup.cs
+++ hwFinanceApp/Startup.cs
@@ -28,7 +28,11 @@ namespace hwFinanceApp
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "hwFinanceApp", Version = "v1" });
             });
-            services.AddDbContext<FinanceContext>(opt => opt.UseInMemoryDatabase("TransactionList"));
+           // services.AddDbContext<FinanceContext>(opt => opt.UseInMemoryDatabase("TransactionList")); --only used when no db around
+            services.AddDbContext<FinanceContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            
             services.AddCors(options =>
             {
                 options.AddPolicy(name: MyAllowSpecificOrigins, builder =>
@@ -39,7 +43,6 @@ namespace hwFinanceApp
                                                                     //.AllowCredentials(); can't use credentials with allowany origin
                                                                 });
             });
-            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
